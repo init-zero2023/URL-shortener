a039bc0579973c9350377f54f378a78f158add0f
init-zero2023 <santosh.sk11112222@gmail.com>
2021-03-02 14:19:11 +0530

    Completed

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ShortUrl = require('./models/shortUrl');
+
 const app = express();
-
-
 mongoose.connect('mongodb://localhost/urlShortener', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async(req, res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
 });
 
-app.post('/shorturls', (req, res) => {
-
+app.post('/shortUrls', async(req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('/');
 });
 
+app.get('/:shortUrl', async(req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null)
+        res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+});
 
 app.listen(3000 || process.env.PORT, () => {
-    console.log("Server started at port 3000");
+    console.log("Port is listening at port 3000");
 });
